323d35cfedc5ad73cd1ba0ae53168ac830850409
Erik Dubois <erik.dubois@gmail.com>
2025-07-08 07:00:22 +0200

    update

--- up.sh
+++ up.sh
@@ -36,6 +36,7 @@ rm "$workdir/archiso/airootfs/etc/pacman.d/mirrorlist"
 touch $workdir/archiso/airootfs/etc/pacman.d/mirrorlist
 echo "## Best Arch Linux servers worldwide
 
+Server = https://mirror.osbeck.com/archlinux/\$repo/os/\$arch
 Server = http://mirror.rackspace.com/archlinux/\$repo/os/\$arch
 Server = https://mirror.rackspace.com/archlinux/\$repo/os/\$arch
 Server = https://mirrors.kernel.org/archlinux/\$repo/os/\$arch
